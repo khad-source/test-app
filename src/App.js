01c3d4810712b301e0e04465a5da6ed088965da8
khad-source <pakimanstore@gmail.com>
2024-02-20 13:06:34 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes,Route,BrowserRouter} from 'react-router-dom'
+import { Navbar } from './Navbar';
+import { Home } from './Home';
+import { About } from './About';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+    </Routes>
+    
+    </BrowserRouter>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router-dom"
+
+
+
+export const Navbar = () => {
+  return (
+    <nav>
+<NavLink to="/">Home</NavLink>
+<NavLink to="/about">about</NavLink>
+
+    </nav>
+  )
+}
+
